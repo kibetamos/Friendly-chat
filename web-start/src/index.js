08c0e0250e3a128306464d9ee01dc5f32e836024
kibet kilel <amos.rotich@strathmore.edu>
2022-02-17 16:32:30 +0300

    Update

--- web-start/src/index.js
+++ web-start/src/index.js
@@ -127,16 +127,64 @@ displayMessage(change.doc.id, message.timestamp, message.name,
 // This first saves the image in Firebase storage.
 async function saveImageMessage(file) {
   // TODO 9: Posts a new image as a message.
+  // 1 - We add a message with a loading icon that will get updated with the shared image.
+  firebase.firestore().collection('messages').add({
+    name: getUserName(),
+    imageUrl: LOADING_IMAGE_URL,
+    profilePicUrl: getProfilePicUrl(),
+    timestamp: firebase.firestore.FieldValue.serverTimestamp()
+  }).then(function(messageRef) {
+    // 2 - Upload the image to Cloud Storage.
+    var filePath = firebase.auth().currentUser.uid + '/' + messageRef.id + '/' + file.name;
+    return firebase.storage().ref(filePath).put(file).then(function(fileSnapshot) {
+      // 3 - Generate a public URL for the file.
+      return fileSnapshot.ref.getDownloadURL().then((url) => {
+        // 4 - Update the chat message placeholder with the image's URL.
+        return messageRef.update({
+          imageUrl: url,
+          storageUri: fileSnapshot.metadata.fullPath
+        });
+      });
+    });
+  }).catch(function(error) {
+    console.error('There was an error uploading a file to Cloud Storage:', error);
+  });
 }
 
 // Saves the messaging device token to Cloud Firestore.
 async function saveMessagingDeviceToken() {
   // TODO 10: Save the device token in Cloud Firestore
+  firebase.messaging().getToken().then(function(currentToken) {
+    if (currentToken) {
+      console.log('Got FCM device token:', currentToken);
+      // Saving the Device Token to the datastore.
+      firebase.firestore().collection('fcmTokens').doc(currentToken)
+          .set({uid: firebase.auth().currentUser.uid});
+    } else {
+      // Need to request permissions to show notifications.
+      requestNotificationsPermissions();
+    }
+  }).catch(function(error){
+    console.error('Unable to get messaging token.', error);
+  });
 }
 
 // Requests permissions to show notifications.
 async function requestNotificationsPermissions() {
   // TODO 11: Request permissions to send notifications.
+  firebase.messaging().getToken().then(function(currentToken) {
+    if (currentToken) {
+      console.log('Got FCM device token:', currentToken);
+      // Saving the Device Token to the datastore.
+      firebase.firestore().collection('fcmTokens').doc(currentToken)
+          .set({uid: firebase.auth().currentUser.uid});
+    } else {
+      // Need to request permissions to show notifications.
+      requestNotificationsPermissions();
+    }
+  }).catch(function(error){
+    console.error('Unable to get messaging token.', error);
+  });
 }
 
 // Triggered when a file is selected via the media picker.
@@ -384,3 +432,12 @@ const firebaseAppConfig = getFirebaseConfig();
 
 initFirebaseAuth();
 loadMessages();
+
+
+
+
+
+
+// AAAAiQgoI1I:APA91bHq9CGkXzB9-qax6PWO2n1ka1dZXtAZAyR3DdVmkP_KNJIepr4JNfoYspvz6dtzePryDjVWounuVEB9DlMh8btsPi9O6u3v3E0TSzpOq3j4IcUAWcft60BUoeFMZq4rLV_zz6HF
+
+//           GET https://lh3.googleusercontent.com/a/AATXAJwY4X1yv70IZ4PFEfUuvpIoASI9NwjqDm6P1M4i=s96-c?sz=150 403
